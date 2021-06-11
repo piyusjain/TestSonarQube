ebdcc4bf567b4ebee2f60f49185f4697ad23cbd6
Piyush Jain <77725666+jainpiy@users.noreply.github.com>
2021-06-11 12:00:58 +0530

    First commit.

---
+++ src/main/java/com/sonar/service/MathService.java
@@ -0,0 +1,13 @@
+package com.sonar.service;
+
+public class MathService {
+    public int multiply(int firstNumber, int secondNumber) {
+
+        return firstNumber * secondNumber;
+    }
+
+    public int subtract(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+}
